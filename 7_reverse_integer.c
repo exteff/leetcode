52796a377750bf58a516e0e77e6e0f56ade43f01
exteff <136258984+exteff@users.noreply.github.com>
2026-02-28 22:40:38 -0500

    Add 7: reverse integer

---
+++ 7_reverse_integer.c
@@ -0,0 +1,12 @@
+int reverse(int x)
+{
+    int rev = 0;
+    while (x) {
+        if (rev < -214748364 || rev > 214748364)
+            return 0;
+        rev *= 10;
+        rev += x % 10;
+        x /= 10;
+    }
+    return rev;
+}
